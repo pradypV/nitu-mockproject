841d95355bfbf8c1830c73d8fe48c26ad646e529
nitu515 <nitu.chaudhary@varicon.com.au>
2023-02-10 13:55:59 +0545

    test

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -9,7 +9,6 @@ import MuiAlert from '@mui/material/Alert';
 const Alert = React.forwardRef(function Alert(props,ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
-
 const Login = ({setAuth}) => {
     const  naviagte = useNavigate();
     const initialState = {
